4b407e372ddb7910a2836c2e3b2aa25b7dfaf90b
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-01-29 19:51:22 +0100

    backend: refactor: move controller into /src

---
+++ backend/src/controller.ts
@@ -0,0 +1,20 @@
+import { Database } from "bun:sqlite";
+
+const db = new Database("mydb.sqlite", { create: true, strict: true });
+
+db.query(
+  `
+  CREATE TABLE IF NOT EXISTS records (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    hex_string TEXT UNIQUE,
+    url TEXT,
+    creation_date DATE DEFAULT CURRENT_DATE
+  );
+`,
+).run();
+
+const api = {
+  insert() {},
+};
+
+export default api;
