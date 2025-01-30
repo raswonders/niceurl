b6b3cdb3bca94baa55c48ea613126f43d017fde1
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-01-30 19:36:03 +0100

    backend: rename table records to urls

--- backend/src/controller.test.ts
+++ backend/src/controller.test.ts
@@ -1,6 +1,17 @@
 import { test, expect } from "bun:test";
+import { Database } from "bun:sqlite";
+
+const dbPath = "../mydb.sqlite";
+const db = new Database(dbPath);
 
 test("database exists", async () => {
-  const dbExists = await Bun.file("../mydb.sqlite").exists();
+  const dbExists = await Bun.file(dbPath).exists();
   expect(dbExists).toBe(true);
+});
+
+test("database has table named urls", () => {
+  const result = db
+    .query(`SELECT name FROM sqlite_master WHERE type="table" AND name="urls";`)
+    .get();
+  expect(result).toEqual({ name: "urls" });
 });
