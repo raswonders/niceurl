c69757bf85aafa407d83a584c3081777a23926e9
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-02-01 16:54:38 +0100

    backend: refactor model tests

--- backend/src/model.test.ts
+++ backend/src/model.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "bun:test";
+import { test, expect, describe, beforeEach } from "bun:test";
 import { Database } from "bun:sqlite";
 import model from "./model";
 
@@ -17,54 +17,46 @@ test("database has table named urls", () => {
   expect(result).toEqual({ name: "urls" });
 });
 
-test("insert a url", () => {
-  const result = model.insert("TESTTEST", "http://example.com");
-  expect(result).toBeGreaterThan(0);
-
-  // clean-up
-  if (typeof result === "number") {
-    db.query(`DELETE FROM urls WHERE id == $id;`).run({ $id: result });
-  }
-});
-
-test("get url for hash", () => {
-  // setup
-  const hash = "TESTTEST";
-  const url = "http://example.com";
-  model.insert(hash, url);
-
-  const result = model.get(hash);
-  expect(result).toBe(url);
-
-  // clean-up
-  db.query(`DELETE FROM urls WHERE hash = $hash;`).run({ $hash: hash });
-});
-
-test("get null for nonexistent hash", () => {
-  const result = model.get("nonexistent");
-  expect(result).toBe(null);
-});
-
-test("delete a hash", () => {
-  // setup
-  const hash = "TESTTEST";
-  const url = "http://example.com";
-  model.insert(hash, url);
-
-  const result = model.delete(hash);
-  expect(result).toBeGreaterThan(0);
-});
-
-test("update url for a hash", () => {
-  // setup
+describe("api", () => {
   const hash = "TESTTEST";
   const url = "http://example.com";
   const url2 = "http://example2.com";
-  model.insert(hash, url);
 
-  expect(model.update(hash, url2)).toBeGreaterThan(0);
-  expect(model.get(hash)).toBe(url2);
+  beforeEach(() => {
+    db.query(`DELETE FROM urls WHERE hash == $hash;`).run({ $hash: hash });
+  });
 
-  // clean-up
-  db.query(`DELETE FROM urls WHERE hash = $hash;`).run({ $hash: hash });
+  test("insert a url", () => {
+    const result = model.insert(hash, url);
+    expect(result).toBeGreaterThan(0);
+  });
+
+  test("get url for hash", () => {
+    // setup
+    model.insert(hash, url);
+
+    const result = model.get(hash);
+    expect(result).toBe(url);
+  });
+
+  test("get null for nonexistent hash", () => {
+    const result = model.get("nonexistent");
+    expect(result).toBe(null);
+  });
+
+  test("delete a hash", () => {
+    // setup
+    model.insert(hash, url);
+
+    const result = model.delete(hash);
+    expect(result).toBeGreaterThan(0);
+  });
+
+  test("update url for a hash", () => {
+    // setup
+    model.insert(hash, url);
+
+    expect(model.update(hash, url2)).toBeGreaterThan(0);
+    expect(model.get(hash)).toBe(url2);
+  });
 });
